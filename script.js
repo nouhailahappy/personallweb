de5272a3b14ebaecbf22ace9ab2f76e6dbb08462
nouhailahappy <you@example.com>
2020-11-12 11:47:01 +0100

    first

---
+++ script.js
@@ -0,0 +1,50 @@
+//nav checkbtn
+function toggle(){
+    var menu = document.getElementById("Menu")
+    var menuoverlay = document.getElementById("menu-overlay")
+    menu.classList.toggle('active')
+    menuoverlay.classList.toggle('active')
+}
+
+function toggle(){
+  var menu = document.getElementById("Menu")
+  var menuoverlay = document.getElementById("menu-overlay")
+  menu.classList.toggle('active')
+  menuoverlay.classList.toggle('active')
+}
+function fonction(){
+  var header = document.getElementById("header")
+  var menuoverlay = document.getElementById("menu-overlay")
+  var menu = document.getElementById("Menu")
+  menuoverlay.classList.remove('active')
+  menu.classList.remove('active')
+  header.classList.add('active')
+}
+
+// scroll reveal 
+window.sr = ScrollReveal();
+sr.reveal(".animate-left", {
+  origin: "left",
+  duration: 1000,
+  distance: "25rem",
+  delay: 300
+});
+
+sr.reveal(".animate-right", {
+  origin: "right",
+  duration: 600,
+  distance: "5rem",
+  delay: 600
+});
+sr.reveal(".animate-top", {
+  origin: "top",
+  duration: 200,
+  distance: "25rem",
+  delay: 600
+});
+sr.reveal(".animate-bottom", {
+  origin: "bottom",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600
+});
